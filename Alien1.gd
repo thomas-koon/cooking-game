418af1960657799fad0483fc3aec4d9e0618282d
thomas-koon <tkoon@ucsd.edu>
2023-05-20 03:03:53 -0700

    added random jump intervals for alien1

--- Alien1.gd
+++ Alien1.gd
@@ -4,9 +4,11 @@ const GRAVITY = 40;
 var velocity = Vector3.ZERO;
 
 onready var player = get_tree().get_nodes_in_group("player")[0];
+onready var jumpTimer: Timer = $JumpTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	jumpTimer.start()
 	pass # Replace with function body.
 
 func _physics_process(delta):
@@ -33,5 +35,10 @@ func _jump():
 	var verticalVelocity = sqrt(2.0 * GRAVITY * jumpHeight)
 	var horizontalVelocity = player_direction.normalized() * distance * 0.25# Adjust the speed as needed
 	velocity = horizontalVelocity + Vector3(0, verticalVelocity, 0)
-	
 
+func _on_JumpTimer_timeout():
+	# do some checks first
+	if is_on_floor():
+		_jump()
+		jumpTimer.wait_time = rand_range(1.0, 3.0)
+		jumpTimer.start()
